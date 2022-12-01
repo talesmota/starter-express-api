c662b5d9c78d58fd67aae62ec71b6ccfd583f979
Tales Mota Machado <talesmmachado@gmail.com>
2022-12-01 20:34:23 -0300

    initial

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const mqtt = require("mqtt");
+
+const CLIENT_URL = "mqtt://broker.hivemq.com";
+const TOPIC = "WLED-MGiA1nky6h";
+const TOKEN = "e0f79dd5-a6de-4e11-9bdf-717fede9ab2d";
+
+const CLIENT = mqtt.connect(CLIENT_URL);
+
+const app = express();
+app.all("/wled", (req, res) => {
+  const { func, color, token } = req.query;
+  if (token !== TOKEN) {
+    res.status(403).send("UNAUTHORIZED");
+  }
+
+  if (func !== undefined) {
+    CLIENT.publish(TOPIC, func.toString());
+  }
+  if (color !== undefined) {
+    CLIENT.publish(`${TOPIC}/col`, color.toString());
+  }
+  res.send({
+    status: "ok",
+    color: color !== undefined ? color : "",
+    func: func !== undefined ? func : "",
+  });
+});
+app.listen(process.env.PORT || 3000);
